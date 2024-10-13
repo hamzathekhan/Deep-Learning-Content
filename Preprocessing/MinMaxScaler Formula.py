bfa0ebf807a660b794d0562f7d433eb161c660b3
Hamza Khalil <142446186+hamzathekhan@users.noreply.github.com>
2024-10-13 16:07:59 +0500

    Create MinMaxScaler Formula.py

---
+++ Preprocessing/MinMaxScaler Formula.py
@@ -0,0 +1,13 @@
+### Here is mathmatical formula for minmax scaler
+
+
+### (Actual_value - Min_value ) / (Max_value - Min_value)
+
+
+Actual_value = 40
+Min_value = 10
+Max_value = 50
+
+MinMaxScaler = (Actual_value - Min_value ) /( Max_value - Min_value)
+
+#### e.g mms = (40-10)/(50-10) #### anwer will be in range of 0 to 1.
